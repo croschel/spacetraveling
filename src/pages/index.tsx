297ba0d277418e4a0cf4ed6affdee99dd1a0c00b
Caique Roschel <croschel000@gmail.com>
2021-04-15 13:42:34 -0300

    Home layout is ready

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { GetStaticProps } from 'next';
 
+import Prismic from '@prismicio/client';
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -23,18 +26,75 @@ interface PostPagination {
 interface HomeProps {
   postsPagination: PostPagination;
 }
-export default function Home() {
+export default function Home(props: HomeProps) {
+  const { postsPagination } = props;
+  const { results, next_page } = postsPagination;
+
+  const loadMorePosts = () => {};
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main className={commonStyles.container}>
+      <div className={styles.postsContainer}>
+        {results.map(post => (
+          <Link key={post.uid} href={`/post/${post.uid}`}>
+            <a>
+              <h2>{post.data.title}</h2>
+              <p>{post.data.subtitle}</p>
+              <div className={styles.contentFooter}>
+                <div>
+                  <FiCalendar />
+                  <time>{post.first_publication_date}</time>
+                </div>
+                <div>
+                  <FiUser />
+                  <p>{post.data.author}</p>
+                </div>
+              </div>
+            </a>
+          </Link>
+        ))}
+        {next_page !== null && (
+          <button type="button" onClick={() => {}}>
+            Carregar mais posts
+          </button>
+        )}
+      </div>
+    </main>
   );
 }
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
-  // const postsResponse = await prismic.query(TODO);
-  // TODO;
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 2,
+    }
+  );
+  // console.log(JSON.stringify(postsResponse));
+  const results = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: new Date(
+        post.first_publication_date
+      ).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
+      }),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
   return {
-    props: {},
+    props: {
+      postsPagination: {
+        results,
+        next_page: postsResponse.next_page,
+      },
+    },
   };
 };
